2066c0b2919600c0f0684c047f76cb134cfa970c
mahamudulhasan-me <mahamudulhasan.org@gmail.com>
2024-07-09 00:59:13 +0600

    add drawer

--- src/components/header/Nav/ResponsieveNav.tsx
+++ src/components/header/Nav/ResponsieveNav.tsx
@@ -1,12 +1,9 @@
-import { PlusIcon } from "lucide-react";
 import { useState } from "react";
 import { Button } from "../../ui/button";
 import {
   Drawer,
-  DrawerClose,
   DrawerContent,
   DrawerDescription,
-  DrawerFooter,
   DrawerHeader,
   DrawerTitle,
   DrawerTrigger,
@@ -37,55 +34,23 @@ const DrawerDemo = () => {
   }
 
   return (
-    <Drawer>
+    <Drawer direction="left">
       <DrawerTrigger asChild>
         <Button variant="outline">Open Drawer</Button>
       </DrawerTrigger>
-      <DrawerContent className="left-0 transform transition-transform duration-300 ease-in-out">
-        <div className="mx-auto w-full max-w-sm">
+      <DrawerContent className="left-0 right-1/2 top-0 transform transition-transform duration-300 ease-in-out">
+        <div>
           <DrawerHeader>
             <DrawerTitle>Move Goal</DrawerTitle>
             <DrawerDescription>Set your daily activity goal.</DrawerDescription>
           </DrawerHeader>
-          <div className="p-4 pb-0">
-            <div className="flex items-center justify-center space-x-2">
-              <Button
-                variant="outline"
-                size="icon"
-                className="h-8 w-8 shrink-0 rounded-full"
-                onClick={() => onClick(-10)}
-                disabled={goal <= 200}
-              >
-                {/* <MinusIcon className="h-4 w-4" /> */}
-                <span className="sr-only">Decrease</span>
-              </Button>
-              <div className="flex-1 text-center">
-                <div className="text-7xl font-bold tracking-tighter">
-                  {goal}
-                </div>
-                <div className="text-[0.70rem] uppercase text-muted-foreground">
-                  Calories/day
-                </div>
-              </div>
-              <Button
-                variant="outline"
-                size="icon"
-                className="h-8 w-8 shrink-0 rounded-full"
-                onClick={() => onClick(10)}
-                disabled={goal >= 400}
-              >
-                <PlusIcon className="h-4 w-4" />
-                <span className="sr-only">Increase</span>
-              </Button>
-            </div>
-            <div className="mt-3 h-[120px]"></div>
-          </div>
-          <DrawerFooter>
+          <p>content</p>
+          {/* <DrawerFooter>
             <Button>Submit</Button>
             <DrawerClose asChild>
               <Button variant="outline">Cancel</Button>
             </DrawerClose>
-          </DrawerFooter>
+          </DrawerFooter> */}
         </div>
       </DrawerContent>
     </Drawer>
