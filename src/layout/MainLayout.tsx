5131d0827e3e93e12b2ef0f450977cc8517f2fe8
mahamudulhasan-me <mahamudulhasan.org@gmail.com>
2024-07-10 12:20:59 +0600

    add news letter box card

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../components/footer/Footer";
 import Header from "../components/header/Header";
+import Newsletter from "../components/newsletter/Newsletter";
 
 const MainLayout = () => {
   return (
     <div>
       <Header />
       <Outlet />
+      <Newsletter />
       <Footer />
     </div>
   );
